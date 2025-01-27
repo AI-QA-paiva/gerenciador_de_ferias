dab02cda40eb5989c738c6e895fe8b12c1b1cdae
Bowie <marketing@letscode.com.br>
2025-01-26 22:24:17 -0300

    versionar primeira estrutura do projeto

---
+++ src/main/java/com/applications/gerenciadordeferias/GerenciadordeferiasApplication.java
@@ -0,0 +1,13 @@
+package com.applications.gerenciadordeferias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordeferiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordeferiasApplication.class, args);
+	}
+
+}
